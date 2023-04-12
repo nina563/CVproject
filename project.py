37a33aa7a14809b05530dc24cc4a5c759e0064ae
nina563 <83282861+nina563@users.noreply.github.com>
2023-04-12 10:49:29 +0200

    Add files via upload

--- project.py
+++ project.py
@@ -8,6 +8,7 @@ from sklearn.linear_model import RANSACRegressor
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 
+
 def extractImages(pathIn):
     parent_dir = os.path.dirname(pathIn)
     folder = 'frames'  # create a folder to store extracted images
@@ -142,9 +143,8 @@ def intersection_points_detect(parameters):
         # Print the intersection point
         # print("Vanishing point: ({}, {})".format(x_intersect, y_intersect))
         points.append((x_intersect, y_intersect))
-
-        cv2.circle(img, (int(x_intersect), int(y_intersect)), 5, (255, 0, 0), -1)
-
+    x_intersect, y_intersect = points[1]
+    cv2.circle(img, (int(x_intersect), int(y_intersect)), 5, (255, 0, 0), -1)
     # cv2.imshow('Chessboard Corners', img)
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
@@ -159,10 +159,10 @@ def distance(point1, point2):# Calculates the distance between two points in a 2
     # print("point2", point2, "x2", x2, "y2", y2)
     return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 
-def focal_length_calc(vp1,vp2,p, p_uv):
-    OcVi_len = math.sqrt(distance(vp1, p_uv ) * distance(p_uv , vp2))
+def focal_length_calc(vp_u,vp_v,p, p_uv):
+    OcVi_len = math.sqrt(distance(vp_v, p_uv ) * distance(p_uv ,vp_u))
     focal_length =math.sqrt( pow(OcVi_len, 2) -pow(distance(p, p_uv), 2))
-    #print("focal_length",focal_length)
+    print("focal_length",focal_length)
     return focal_length
 
 def principal_point_coordinates(img):
@@ -175,52 +175,45 @@ def principal_point_coordinates(img):
     #print("Center point: ({}, {})".format(center_x, center_y))
     return [center_x, center_y]
 
-def intrinsic_parameters(img, vp1, vp2):
+def intrinsic_parameters(img, vp_u, vp_v):
     p_point = principal_point_coordinates(img)  # u0, v0 for the intrinsic matrix
     u0= p_point[0]
     v0= p_point[1]
     skew = 0
     scale_factor_u = scale_factor_v = 1
     # Convert points to numpy arrays
-    vp1 = np.array(vp1)
-    vp2 = np.array(vp2)
+    vp_u = np.array(vp_u)
+    vp_v = np.array(vp_v)
     p = np.array(p_point)
 
-    # Calculate the unit vector of P1P2
-    vp1vp2 = vp2 - vp1
-    vp1vp2_unit = vp1vp2 / np.linalg.norm(vp1vp2)
-    # Calculate the vector projection of OP onto P1P2
-    pvp1 = p - vp1
-    op_proj = np.dot(pvp1, vp1vp2_unit) * vp1vp2_unit
-    # Calculate the coordinates of point P
-    # Puv be the orthogonal projection of P on the the line (VP1 VP2)
-    p_uv = vp1 + op_proj
-
-    f=focal_length_calc(vp1,vp2,p, p_uv) # focal length
+    ap = p_point - vp_v
+    ab = vp_u - vp_v
+    p_uv= vp_v + np.dot(ap, ab) / np.dot(ab, ab) * ab
+    print("p_uv",p_uv)
+    f=focal_length_calc(vp_u,vp_v,p, p_uv) # focal length
     K =np.array([[scale_factor_u*f , skew,            u0],
-        [0,                 scale_factor_v*f, v0],
-        [0,                   0,               1]])
+                [0,                scale_factor_v*f, v0],
+                [0,                0,                1]])
     # print("intrinsic matrix", K)
     return f, K
 
 
-def rotation(intersection_points, f):
-    vp1 = intersection_points[0]
-    vp2 = intersection_points[1]
+def rotation(vp_u, vp_v, f):
 
-    len_u_rc = math.sqrt(vp1[0]**2 + vp1[1]**2+ f**2)
-    len_v_rc = math.sqrt(vp2[0] ** 2 + vp2[1] ** 2 + f ** 2)
-    v_rc = np.array([vp1[0],vp1[1], f]) / len_v_rc
-    u_rc = np.array([vp2[0], vp2[1], f]) / len_u_rc
+    len_u_rc = math.sqrt(vp_u[0]**2 + vp_u[1]**2 + f**2)
+    len_v_rc = math.sqrt(vp_v[0] ** 2 + vp_v[1] ** 2 + f ** 2)
+    u_rc = np.array([vp_u[0], vp_u[1], f]) / len_u_rc
+    v_rc = np.array([vp_v[0],vp_v[1], f]) / len_v_rc
+
     w_rc =np.cross(u_rc, v_rc)
 
-    v_rc = v_rc.reshape(3,1)
     u_rc= u_rc.reshape(3,1)
+    v_rc = v_rc.reshape(3,1)
     w_rc= w_rc.reshape(3,1)
 
     # rotation from world to camera
     rotation = np.hstack((u_rc, v_rc, w_rc))
-    #print("rotation", rotation)
+    print("rotation", rotation)
     return rotation
 
 # point1 and point2 are coordinates of the side of the pattern in world coordinates
@@ -235,21 +228,20 @@ def construct_plane(p1, p2, p3):
     d = -np.dot(normal, np.array(p1))
     return np.concatenate((normal, [d])), normal
 
-def translation(f, img, r, point1, point2, all_corners, square_size, pattern_size ):
+def translation(vp_u, f, img, r, all_corners, square_size, pattern_size ):
     x, y = pattern_size
     principal_point = principal_point_coordinates(img)  # image coordinates of the principal point
 
     k_im = all_corners[int(x/2 -1)] # coordinates of k' in the pixel coordinates, k' lies on the projection line a'd'
     # define the a' and d' points - projections of the longest side of the pattern to the image plane
     a_im = all_corners[int(x - 1)]
-    d_im =all_corners[0]
     k_u, k_v = k_im # x, y in pixels
-    # x*1.8*2.54/ 3.07
-    # y*1* 2.54/ 2.048
     p_u, p_v = principal_point
     a_u, a_v = a_im
-    k_rc = np.array([ (k_u - p_u)*1.8*2.54/ 3.07, (k_v - p_v)*1* 2.54/ 2.048, f])
-    a_rc = np.array([ (a_u - p_u)*1.8*2.54/ 3.07,(a_v - p_v)*1* 2.54/ 2.048, f])
+    k_rc = np.array([ (k_u - p_u), (k_v - p_v), f])
+    a_rc = np.array([ (a_u - p_u),(a_v - p_v), f])
+    # k_rc = np.array([ (k_u - p_u)*1.8*2.54/ 3.07, (k_v - p_v)*1* 2.54/ 2.048, f])
+    # a_rc = np.array([ (a_u - p_u)*1.8*2.54/ 3.07,(a_v - p_v)*1* 2.54/ 2.048, f])
     o_rc = np.zeros(3)
 
     #  each square is 125 mm for the A0 pattern
@@ -258,10 +250,14 @@ def translation(f, img, r, point1, point2, all_corners, square_size, pattern_siz
     print("AK_ro", AK_ro)
     AK_rc =np.dot(r,AK_ro).reshape(3,) #AP_rc = M_oc*AP_ro ----d line
     print("AK_rc", AK_rc)
-    k_new = -10*AK_rc+a_rc
-    print("k new", k_new)
     AK_rc_unit= AK_rc / np.linalg.norm(AK_rc) # unit vector
-    AK_rc_from_a = a_rc -100*AK_rc_unit
+    #k_new = a_rc+ a_rc- AK_rc
+    #print("k_new", k_new)
+    print("AK_rc_unit", AK_rc_unit)
+    ovp_u = (np.array([vp_u[0],vp_u[1],f ]))/ np.linalg.norm(np.array([vp_u[0],vp_u[1],f ]))
+    print("ovp_u", ovp_u)
+
+    AK_rc = a_rc+ AK_rc
 
     print("a_rc",  a_rc)
     print("k_rc",  k_rc)
@@ -273,20 +269,26 @@ def translation(f, img, r, point1, point2, all_corners, square_size, pattern_siz
     plane_norm_unit = n / np.linalg.norm(n)
     print("normal unit", plane_norm_unit)
 
-    # AP_proj = project_onto_plane(AK_rc, normal_unit, o_rc)
-    # p_intersection_KO = intersection_lines(AP_proj, o_rc, k_rc)
-    # print("p_intersection_KO", p_intersection_KO)
+    # AP_proj = project_onto_plane(AK_rc, plane_norm_unit, o_rc)
+    # print("AP_proj", AP_proj)
+    # p_new = AP_proj+a_rc
+    # print("p_new", p_new)
+    p_intersection_KO = intersection_lines(a_rc, AK_rc , o_rc, k_rc)
+    print("p_intersection_KO", p_intersection_KO)
+    #
+    # distance_plane_to_vector = np.abs(AK_rc.dot(plane_norm_unit))
+    # print("distance_plane_to_vector", distance_plane_to_vector)
 
-    distance_plane_to_vector = np.abs(AK_rc.dot(plane_norm_unit))
-    print("distance_plane_to_vector", distance_plane_to_vector)
+
     # Find the dot product between the vector and normal vector of the plane
-    dot_prod = np.dot(AK_rc_from_a, plane_norm_unit)
+    dot_prod = np.dot((-AK_rc), plane_norm_unit)
     # Calculate the magnitudes of the vector and normal vector of the plane
-    vec_mag = np.linalg.norm(AK_rc_from_a)
+    vec_mag = np.linalg.norm((-AK_rc))
     normal_mag = np.linalg.norm(plane_norm_unit)
     # Calculate the angle between the vector and plane
     angle = np.arccos(dot_prod / (vec_mag * normal_mag))
     print("angle", angle)
+
 
 
 #VISUALIZATION
@@ -303,42 +305,61 @@ def translation(f, img, r, point1, point2, all_corners, square_size, pattern_siz
     # ax.quiver(o_rc[0], o_rc[1], o_rc[2], normal_from_o[0], normal_from_o[1], normal_from_o[2], color='b')  # ok_rc
 
     #Plot the vector as an arrow
-    ax.quiver(a_rc[0], a_rc[1], a_rc[2], AK_rc[0], AK_rc[1], AK_rc[2], color='r') # ok_rc
+    ax.quiver(a_rc[0], a_rc[1], a_rc[2], -AK_rc[0], -AK_rc[1], -AK_rc[2], color='r') # ok_rc
 
     #Plot the points as spheres
     ax.scatter(a_rc[0], a_rc[1], a_rc[2], color='b', marker='o')
     ax.scatter(k_rc[0], k_rc[1], k_rc[2], color='b', marker='o')
     ax.scatter(o_rc[0], o_rc[1], o_rc[2], color='b', marker='o')
-    ax.scatter(k_new[0], k_new[1], k_new[2], color='r', marker='o')
+    #ax.scatter(k_new[0], k_new[1], k_new[2], color='r', marker='o')
+
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
 
     return None
 
-# def project_onto_plane(v, normal,o):
-#     # Compute the dot product of the normal vector with the vector you want to project.
-#     signed_distance = np.dot(normal, v - o)
-#     # Subtract the projection of the vector onto the normal vector from the original vector-  Calculate the projection of vector AP onto the plane OAK.
-#     projection = v - signed_distance / np.dot(normal, normal) * normal
-#     return projection
+def project_onto_plane(v, normal,o):
+    # Compute the dot product of the normal vector with the vector you want to project.
+    signed_distance = np.dot(normal, v - o)
+    # Subtract the projection of the vector onto the normal vector from the original vector-  Calculate the projection of vector AP onto the plane OAK.
+    projection = v - signed_distance / np.dot(normal, normal) * normal
+    return projection
 
-def intersection_lines(projection, o_rc, k_rc):
-    # Find the equation of the line KO.
-    # The line passes through K and O, so we can write it as K + t*(O-K).
-    # Here, we define the direction vector d = O - K.
-    d = o_rc - k_rc
-    #  Find the point of intersection P between the line KO and the projected vector from step 2.
-    # We substitute the equation of the line into the equation for the projected vector, and solve for t.
-    t = np.dot(projection - k_rc, d) / np.dot(d, d)
-    # Finally, we calculate the point of intersection P as K + t*(O-K).
-    P = k_rc + t*d
-    # The point P is the intersection of the line KO and the projection of vector AP onto the plane OAK.
-    return P
+def intersection_lines(a_rc, p_new, o_rc, k_rc):
+    intersection = None
+    # Define the two 3D lines
+    # Line 1: x = 1 + t, y = 2 + 2t, z = 3 + 3t
+    # Line 2: x = 2 + s, y = 1 + s, z = 4 + 2s
+    p1 = np.array(p_new)
+    d1 = np.array(p_new-a_rc)
+    p2 = np.array(k_rc)
+    d2 = np.array(k_rc -o_rc)
 
-def point_on_line_segment_3d(p1, p2, test_point):
-    d = abs(np.cross((p2-p1), (p1-test_point))) / abs(p2-p1)
-    print("d", d)
+    # Find the vector connecting the two points
+    v = p2 - p1
+
+    # Find the normal vector of the plane
+    n = np.cross(d1, d2)
+    print("cross product", np.cross(p1,p2))
+    print("norm of d1 d2 ",n /np.linalg.norm(n))
+    # Find the distance between the two lines
+    dist = np.dot(v, n) / np.linalg.norm(n)
+
+    # If the distance is zero, the two lines intersect
+    if dist == 0:
+        # Find the parameter t for Line 1
+        t = np.dot(v, np.cross(p2 - p1, d2)) / np.linalg.norm(np.cross(d1, d2)) ** 2
+        # Find the point of intersection
+        intersection = p1 + t * d1
+        print("The two lines intersect at point", intersection)
+    else:
+        print("The two lines do not intersect.")
+    return intersection
+
+# def point_on_line_segment_3d(p1, p2, test_point):
+#     d = abs(np.cross((p2-p1), (p1-test_point))) / abs(p2-p1)
+#     print("d", d)
 
 if __name__ == "__main__":
     import matplotlib
@@ -350,7 +371,9 @@ if __name__ == "__main__":
     parimeter_lines_detect(img, pattern_size, all_corners)
     fitted_lines_parameters = line_fit_ransac(img, pattern_size, all_corners)  # [[slop, intercept],...]
     intersection_points = intersection_points_detect(fitted_lines_parameters)
-    f, K = intrinsic_parameters(img, intersection_points[0], intersection_points[1])
-    rotation_matrix = rotation(intersection_points, f)
-    t = translation(f, img, rotation_matrix,intersection_points[0], intersection_points[1], all_corners, square_size_big, pattern_size  )
+    vp_u = intersection_points[0]
+    vp_v = intersection_points[1]
+    f, K = intrinsic_parameters(img, vp_u, vp_v)
+    rotation_matrix = rotation(vp_u, vp_v, f)
+    t = translation(vp_u, f, img, rotation_matrix, all_corners, square_size_big, pattern_size  )
     plt.show(block=True)
