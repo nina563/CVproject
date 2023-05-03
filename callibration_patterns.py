e5d3786ec73b9478af57e55fe86bc8e029d9bae1
nina563 <83282861+nina563@users.noreply.github.com>
2023-05-03 08:50:08 +0200

    added extra info about the pattern

--- callibration_patterns.py
+++ callibration_patterns.py
@@ -1,3 +1,5 @@
 # square_size in mm
 # dimension is in number of squares
-checkered_board = {"dimension": (8, 5), "square_size": 125}
+# size in mm - full size of the pattern
+# x_blank and y_blank in mm  - white spaces around the checkerboard
+checkered_board = {"dimension": (8, 5), "square_size": 125, "size":(1189, 841), "x_blank": 16.5, "y_blank": 59.5}
