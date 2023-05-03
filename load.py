36b753fc0718722769f8dd9020b44a9475c5c058
nina563 <83282861+nina563@users.noreply.github.com>
2023-05-03 08:48:57 +0200

    Add files via upload

---
+++ load.py
@@ -0,0 +1,19 @@
+import os
+import cv2
+
+def load_images():
+    """returns a dictionary of 6 images with their file names as keys """
+    folder_path =os.path.join(os.getcwd(), "tests", "one_image_per_camera")
+    # retrieve the names of all files in the folder
+    file_names = os.listdir(folder_path)
+    image_names = [name for name in file_names]
+    # sort the image names
+    sorted_names = sorted(image_names)
+    # load the images and store them in a dictionary
+    images = {}
+    for name in sorted_names:
+        path = os.path.join(folder_path, name)
+        image = cv2.imread(path)
+        images[name] = image
+    return images
+
