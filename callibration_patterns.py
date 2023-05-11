27bd9be4e1c2f3d5bd5f65905fcacfa9e76d0657
Alena Bruck <63925029+ninulechka123@users.noreply.github.com>
2023-05-11 18:44:46 +0200

    position added

--- callibration_patterns.py
+++ callibration_patterns.py
@@ -5,9 +5,9 @@
 # u_pair - which lines should be used to detect vanishing point that lies on the right side
 # v_pair - which lines should be used to detect vanishing point that lies on the left side
 checkered_board = {"dimension": (8, 5), "square_size": 125, "size":(1189, 841), "x_blank": 31, "y_blank": 44}
-cam1 = {"start_pair":("D", "C"), "u_pair":(2,3), "v_pair":(0,1)}
-cam2 ={"start_pair":("A", "D"), "u_pair":(0,1), "v_pair":(2,3)}
-cam3 = {"start_pair":("A", "D"),"u_pair":(0,1), "v_pair":(2,3)}
-cam4 ={"start_pair":("B", "A"), "u_pair":(2,3), "v_pair":(0,1)}
-cam5 = {"start_pair":("C", "B"), "u_pair":(0,1), "v_pair":(2,3)}
-cam6 ={"start_pair":("D", "C"), "u_pair":(2,3), "v_pair":(0,1)}
+cam1 = {"start_pair":("D", "C"), "u_pair":(2,3), "v_pair":(0,1), "position":3}
+cam2 ={"start_pair":("A", "D"), "u_pair":(0,1), "v_pair":(2,3), "position":3}
+cam3 = {"start_pair":("A", "D"),"u_pair":(0,1), "v_pair":(2,3), "position":2}
+cam4 ={"start_pair":("B", "A"), "u_pair":(2,3), "v_pair":(0,1), "position":1}
+cam5 = {"start_pair":("C", "B"), "u_pair":(0,1), "v_pair":(2,3), "position":1}
+cam6 ={"start_pair":("D", "C"), "u_pair":(2,3), "v_pair":(0,1), "position":2}
