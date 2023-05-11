f678a961578dce3fa84c464583ac2cfff7347547
Alena Bruck <63925029+ninulechka123@users.noreply.github.com>
2023-05-11 13:49:13 +0200

    A and D pair for each camera added

--- callibration_patterns.py
+++ callibration_patterns.py
@@ -3,3 +3,9 @@
 # size in mm - full size of the pattern
 # x_blank and y_blank in mm  - white spaces around the checkerboard
 checkered_board = {"dimension": (8, 5), "square_size": 125, "size":(1189, 841), "x_blank": 31, "y_blank": 44}
+cam1 = {"start_pair":("D", "C")}
+cam2 ={"start_pair":("A", "D")}
+cam3 = {"start_pair":("A", "D")}
+cam4 ={"start_pair":("B", "A")}
+cam5 = {"start_pair":("C", "B")}
+cam6 ={"start_pair":("D", "C")}
