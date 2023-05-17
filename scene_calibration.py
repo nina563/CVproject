74d58c1eb95296d386d63a78333712e60c31fbf9
Alena Bruck <63925029+ninulechka123@users.noreply.github.com>
2023-05-17 09:14:13 +0200

    global extrinsic finished

--- scene_calibration.py
+++ scene_calibration.py
@@ -2,7 +2,77 @@ import numpy as np
 
 from camera_calibration.vanishing_points import get_distance_to_calibration_pattern, get_rotation
 from load import load_images
-from callibration_patterns import checkered_board
+from callibration_patterns import checkered_board, cam1, cam2, cam3, cam4, cam5, cam6
+
+
+def floor_map(_pattern):
+    x_pattern_size, y_pattern_size = _pattern["size"]
+    x_blank = _pattern["x_blank"]
+    y_blank = _pattern["y_blank"]
+    distance_between_patterns = 3000  # 3 m = 3000 mm
+    pattern_actual_x = x_pattern_size - 2 * x_blank
+    pattern_actual_y = y_pattern_size - 2 * y_blank
+    D_edge_coordinate_3_position = np.array([2000, 2000, 0])  # 2m , 2m ,0 x, y, z
+    A_edge_coordinate_3_position = D_edge_coordinate_3_position - np.array([x_pattern_size, y_pattern_size, 0])
+    A_pattern_coordinate_3_position = A_edge_coordinate_3_position + np.array([x_blank, y_blank, 0])
+    B_pattern_coordinate_3_position = A_pattern_coordinate_3_position + np.array([0, pattern_actual_y, 0])
+    C_pattern_coordinate_3_position = B_pattern_coordinate_3_position + np.array([pattern_actual_x, 0, 0])
+    D_pattern_coordinate_3_position = A_pattern_coordinate_3_position + np.array([pattern_actual_x, 0, 0])
+
+    A_edge_coordinate_2_position = A_edge_coordinate_3_position + np.array(
+        [0, y_pattern_size + distance_between_patterns, 0])
+    A_pattern_coordinate_2_position = A_edge_coordinate_2_position + np.array([x_blank, y_blank, 0])
+    B_pattern_coordinate_2_position = A_pattern_coordinate_2_position + np.array([0, pattern_actual_y, 0])
+    C_pattern_coordinate_2_position = B_pattern_coordinate_2_position + np.array([pattern_actual_x, 0, 0])
+    D_pattern_coordinate_2_position = A_pattern_coordinate_2_position + np.array([pattern_actual_x, 0, 0])
+
+    A_edge_coordinate_1_position = A_edge_coordinate_2_position + np.array(
+        [0, y_pattern_size + distance_between_patterns, 0])
+    A_pattern_coordinate_1_position = A_edge_coordinate_1_position + np.array([x_blank, y_blank, 0])
+    B_pattern_coordinate_1_position = A_pattern_coordinate_1_position + np.array([0, pattern_actual_y, 0])
+    C_pattern_coordinate_1_position = B_pattern_coordinate_1_position + np.array([pattern_actual_x, 0, 0])
+    D_pattern_coordinate_1_position = A_pattern_coordinate_1_position + np.array([pattern_actual_x, 0, 0])
+
+    array_of_points = np.array([A_pattern_coordinate_3_position,
+                                B_pattern_coordinate_3_position,
+                                C_pattern_coordinate_3_position,
+                                D_pattern_coordinate_3_position,
+
+                                A_pattern_coordinate_2_position,
+                                B_pattern_coordinate_2_position,
+                                C_pattern_coordinate_2_position,
+                                D_pattern_coordinate_2_position,
+
+                                A_pattern_coordinate_1_position,
+                                B_pattern_coordinate_1_position,
+                                C_pattern_coordinate_1_position,
+                                D_pattern_coordinate_1_position])
+    return array_of_points
+
+def get_coordinates_from_point_names(point_name,_pattern):
+    array_points_name = np.array([
+        "A_pattern_coordinate_3_position",
+        "B_pattern_coordinate_3_position",
+        "C_pattern_coordinate_3_position",
+        "D_pattern_coordinate_3_position",
+        "A_pattern_coordinate_2_position",
+        "B_pattern_coordinate_2_position",
+        "C_pattern_coordinate_2_position",
+        "D_pattern_coordinate_2_position",
+        "A_pattern_coordinate_1_position",
+        "B_pattern_coordinate_1_position",
+        "C_pattern_coordinate_1_position",
+        "D_pattern_coordinate_1_position"
+    ])
+    array_of_floor_points = floor_map(_pattern)
+    index = np.where(array_points_name == point_name)[0]
+    if index.size > 0:
+        coordinate_system_start = array_of_floor_points[index[0]]
+        print("start of the coordinate system - ", point_name,coordinate_system_start )
+    else:
+        print("Value not found in the array.")
+
+    return coordinate_system_start
 
 
 def get_extrinsic_matrix_per_camera(img,image_name, pattern):
@@ -14,42 +84,67 @@ def get_extrinsic_matrix_per_camera(img,image_name, pattern):
     extrinsic_matrix[:3,3] = translation_vector
     return extrinsic_matrix
 
-def get_global_transform_per_camera(camera_name, pattern):
-    camera_number = int(camera_name.split(".")[0][-1])
-    x_pattern_size, y_pattern_size = pattern["size"]
-    x_blank = pattern["x_blank"]
-    y_blank = pattern["y_blank"]
-    distance_between_patterns = 3000 # 3 m = 3000 mm
+def local_rotation(angle):
+    tol = 1e-8
+    cos_val = np.cos(angle)
+    sin_val = np.sin(angle)
 
+    # Set values close to zero as zero
+    if np.abs(cos_val) < tol:
+        cos_val = 0.0
+    if np.abs(sin_val) < tol:
+        sin_val = 0.0
+
+    rotation = np.array([[cos_val, -sin_val, 0],
+                         [sin_val, cos_val , 0],
+                         [0              , 0               , 1]])
+    return rotation
+
+
+def rotation_matrix(start_coordinate_system):
+    rotation_matrix = np.identity(4)
+    if start_coordinate_system == "A":
+        print("0")
+        rotation_matrix[:3,:3] = local_rotation(0)
+    elif start_coordinate_system == "B":
+        print("90 counterclockwise ")
+        rotation_matrix[:3, :3] = local_rotation(np.pi/2)
+    elif start_coordinate_system == "C":
+        print("180 counterclockwise")
+        rotation_matrix[:3,:3] = local_rotation(np.pi)
+    else:
+        print("270 counterclockwise")
+        rotation_matrix[:3,:3] = local_rotation(3*np.pi/2)
+
+    return rotation_matrix
+
+
+
+def get_global_transform_per_camera(point_name, pattern):
     translation_matrix = np.identity(4)
-    D_edge_coordinate_3_position = np.array([2000, 2000, 0]) # 2m , 2m ,0
-    A_edge_coordinate_3_position = D_edge_coordinate_3_position - np.array([x_pattern_size, y_pattern_size, 0])
-    A_pattern_coordinate_3_position = A_edge_coordinate_3_position + np.array([x_blank, y_blank, 0])
-
-    A_edge_coordinate_2_position = A_edge_coordinate_3_position + np.array([0,y_pattern_size+distance_between_patterns, 0])
-    A_pattern_coordinate_2_position = A_edge_coordinate_2_position + np.array([x_blank, y_blank, 0])
-
-    A_edge_coordinate_1_position = A_edge_coordinate_2_position + np.array([0,y_pattern_size+distance_between_patterns, 0])
-    A_pattern_coordinate_1_position = A_edge_coordinate_1_position + np.array([x_blank, y_blank, 0])
-
-    #3rd position of the pattern
-    if (camera_number == 1) or (camera_number == 2):
-        translation_matrix[:3,3] = - A_pattern_coordinate_3_position
-
-    # 2nd position of the pattern
-    if (camera_number == 3) or (camera_number == 6):
-        translation_matrix[:3,3] = - A_pattern_coordinate_2_position
-
-    # 1st position of the pattern
-    if (camera_number == 4) or (camera_number == 5):
-        translation_matrix[:3,3] = - A_pattern_coordinate_1_position
+    coordinate_system_start = get_coordinates_from_point_names(point_name, pattern)
+    translation_matrix[:3, 3] = - coordinate_system_start
 
     return translation_matrix
 
 def get_global_extrisic_matrix(img, image_name,pattern):
     extrinsic = get_extrinsic_matrix_per_camera(img,image_name, pattern)
-    transform_from_local_to_global = get_global_transform_per_camera(image_name, pattern)
-    global_extrinsic_matrix = np.matmul(transform_from_local_to_global, extrinsic)
+
+    camera_name = image_name.split('.')[0]
+    position = globals()[camera_name]["position"] # 1,2,3 position on the floor
+    starting_pair = globals()[camera_name]["start_pair"]
+    letter_start_coordinate_system = starting_pair[0]
+
+    #name of the point on the floor , that is our start of coordinate system
+    point_name = letter_start_coordinate_system + "_pattern_coordinate_" + str(position) + "_position"
+    print("start of the coordinate system", point_name)
+
+
+    transform_from_local_to_global = get_global_transform_per_camera(point_name, pattern)
+    rotation_local_to_global  = rotation_matrix(letter_start_coordinate_system)
+
+    global_extrinsic_matrix = np.matmul(transform_from_local_to_global,np.matmul(rotation_local_to_global, extrinsic))
+    print("extrinsic_matrix of camera", camera_name, "-\n", global_extrinsic_matrix, "\n" )
     return global_extrinsic_matrix
 
 def global_extrisic_matrix_for_all():
@@ -66,3 +161,4 @@ if __name__ == '__main__':
     _pattern = checkered_board
 
     global_extrinsic_matrix = get_global_extrisic_matrix(images["cam2.jpg"],"cam2.jpg", _pattern)
+    global_extrisic_matrix_for_all()
--- tests/camera_calibration_scene_calibration.py
+++ tests/camera_calibration_scene_calibration.py
@@ -1,67 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 import matplotlib
-import cv2
 from callibration_patterns import checkered_board, cam1, cam2, cam3, cam4, cam5, cam6
 from load import load_images
-from camera_calibration.vanishing_points import principal_point_coordinates
-
+from scene_calibration import floor_map, rotation_matrix, get_coordinates_from_point_names, get_extrinsic_matrix_per_camera, get_global_transform_per_camera
 import matplotlib.pyplot as plt
 
-
-
-
-def floor_map(_pattern):
-    x_pattern_size, y_pattern_size = _pattern["size"]
-    x_blank = _pattern["x_blank"]
-    y_blank = _pattern["y_blank"]
-    distance_between_patterns = 3000  # 3 m = 3000 mm
-    pattern_actual_x = x_pattern_size - 2 * x_blank
-    pattern_actual_y = y_pattern_size - 2 * y_blank
-    D_edge_coordinate_3_position = np.array([2000, 2000, 0])  # 2m , 2m ,0 x, y, z
-    A_edge_coordinate_3_position = D_edge_coordinate_3_position - np.array([x_pattern_size, y_pattern_size, 0])
-    A_pattern_coordinate_3_position = A_edge_coordinate_3_position + np.array([x_blank, y_blank, 0])
-    B_pattern_coordinate_3_position = A_pattern_coordinate_3_position + np.array([0, pattern_actual_y, 0])
-    C_pattern_coordinate_3_position = B_pattern_coordinate_3_position + np.array([pattern_actual_x, 0, 0])
-    D_pattern_coordinate_3_position = A_pattern_coordinate_3_position + np.array([pattern_actual_x, 0, 0])
-
-    A_edge_coordinate_2_position = A_edge_coordinate_3_position + np.array(
-        [0, y_pattern_size + distance_between_patterns, 0])
-    A_pattern_coordinate_2_position = A_edge_coordinate_2_position + np.array([x_blank, y_blank, 0])
-    B_pattern_coordinate_2_position = A_pattern_coordinate_2_position + np.array([0, pattern_actual_y, 0])
-    C_pattern_coordinate_2_position = B_pattern_coordinate_2_position + np.array([pattern_actual_x, 0, 0])
-    D_pattern_coordinate_2_position = A_pattern_coordinate_2_position + np.array([pattern_actual_x, 0, 0])
-
-    A_edge_coordinate_1_position = A_edge_coordinate_2_position + np.array(
-        [0, y_pattern_size + distance_between_patterns, 0])
-    A_pattern_coordinate_1_position = A_edge_coordinate_1_position + np.array([x_blank, y_blank, 0])
-    B_pattern_coordinate_1_position = A_pattern_coordinate_1_position + np.array([0, pattern_actual_y, 0])
-    C_pattern_coordinate_1_position = B_pattern_coordinate_1_position + np.array([pattern_actual_x, 0, 0])
-    D_pattern_coordinate_1_position = A_pattern_coordinate_1_position + np.array([pattern_actual_x, 0, 0])
-
-    array_of_points = np.array([A_pattern_coordinate_3_position,
-                                B_pattern_coordinate_3_position,
-                                C_pattern_coordinate_3_position,
-                                D_pattern_coordinate_3_position,
-
-                                A_pattern_coordinate_2_position,
-                                B_pattern_coordinate_2_position,
-                                C_pattern_coordinate_2_position,
-                                D_pattern_coordinate_2_position,
-
-                                A_pattern_coordinate_1_position,
-                                B_pattern_coordinate_1_position,
-                                C_pattern_coordinate_1_position,
-                                D_pattern_coordinate_1_position])
-    return array_of_points
 
 def map_test(pattern):
 
     array_of_floor_points= floor_map(pattern)
 
-    for i in array_of_floor_points:
-        print(i[0], i[1], "\n")
-    # Extract coordinates from the points
     x = [point[0] for point in array_of_floor_points]
     y = [point[1] for point in array_of_floor_points]
 
@@ -79,7 +29,7 @@ def map_test(pattern):
     plt.show()
 
 
-def visual_test(image, image_name, _pattern):
+def test(image, image_name, _pattern):
     camera_name = image_name.split('.')[0]
     position = globals()[camera_name]["position"] # 1,2,3 position on the floor
     starting_pair = globals()[camera_name]["start_pair"]
@@ -89,6 +39,8 @@ def visual_test(image, image_name, _pattern):
     point_name = start_coordinate_system + "_pattern_coordinate_" + str(position) + "_position"
     print(point_name)
 
+    local_to_global_rotation = rotation_matrix(start_coordinate_system)
+    print("local_to_global_rotation\n", local_to_global_rotation)
 
     array_of_floor_points = floor_map(_pattern)
 
@@ -112,10 +64,50 @@ def visual_test(image, image_name, _pattern):
     if index.size > 0:
         coordinate_system_start = array_of_floor_points[index[0]]
         print("start of the coordinate system - ", point_name,coordinate_system_start,"\n" )
+
     else:
         print("Value not found in the array.")
 
+def visual_test(image, image_name, pattern):
+    camera_name = image_name.split('.')[0]
+    position = globals()[camera_name]["position"] # 1,2,3 position on the floor
+    starting_pair = globals()[camera_name]["start_pair"]
+    letter_start_coordinate_system = starting_pair[0]
 
+    #name of the point on the floor , that is our start of coordinate system
+    point_name = letter_start_coordinate_system + "_pattern_coordinate_" + str(position) + "_position"
+    print(point_name)
+    coordinate_system_start = get_coordinates_from_point_names(point_name, pattern)
+
+
+    array_of_floor_points= floor_map(_pattern)
+    x = [point[0] for point in array_of_floor_points]
+    y = [point[1] for point in array_of_floor_points]
+
+    # Create a scatter plot
+    plt.scatter(x, y, c = 'b')
+    plt.scatter(coordinate_system_start[0], coordinate_system_start[1], c = 'r')
+
+    # Set labels and title
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title('2D Scatter Plot')
+    # Set aspect ratio
+    plt.gca().set_aspect('equal', adjustable='box')
+
+    # Show the plot
+    plt.show()
+
+    extrinsic = get_extrinsic_matrix_per_camera(image,image_name, pattern)
+    print("Local extrinsic matrix\n",extrinsic )
+
+    transform_from_local_to_global = get_global_transform_per_camera(point_name, pattern)
+    print("Transformation from local to global \n", transform_from_local_to_global)
+    rotation_local_to_global  = rotation_matrix(letter_start_coordinate_system)
+    print("Rotation from local to global \n", rotation_local_to_global)
+
+    global_extrinsic_matrix = np.matmul(transform_from_local_to_global,np.matmul(rotation_local_to_global, extrinsic))
+    print("extrinsic_matrix of camera", camera_name, "-\n", global_extrinsic_matrix, "\n" )
 
 
 
@@ -130,17 +122,5 @@ if __name__ == '__main__':
         image_name = key # for example : cam6.jpg
         image = images[image_name]
         visual_test(image, image_name, _pattern)
-        # x, y, z = get_distance_to_calibration_pattern(image,image_name, _pattern)
-        # assert ((vt_oa_ro - np.linalg.norm([x, y, z])) < 1e-12)
-        #
-        # print(f"Distance to the wall: {y/1000:0.2f}m, "
-        #       f"distance from the focal axis: {x/1000:0.2f}m, "
-        #       f"camera installation height: {z/1000:0.2f}m")
-        # print("\n")
-
-    plt.show(block=True)
-
-
-
 
     plt.show(block=True)
