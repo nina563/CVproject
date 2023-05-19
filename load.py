0c5be3f71b0362f180700a6e0e1beb6b42dc3bfc
Alena Bruck <63925029+ninulechka123@users.noreply.github.com>
2023-05-19 14:39:56 +0200

    loading without absolute path

--- load.py
+++ load.py
@@ -3,17 +3,24 @@ import cv2
 
 def load_images():
     """returns a dictionary of 6 images with their file names as keys """
-    folder_path = '/Users/nina/Desktop/Project/project_after_luis/one_image_per_camera'
+    # folder_path = '/Users/nina/Desktop/Project/project_after_luis/one_image_per_camera'
+    absolute_path = os.path.dirname(__file__)
+    relative_path = "one_image_per_camera"
+    full_path = os.path.join(absolute_path, relative_path)
+
     # retrieve the names of all files in the folder
-    file_names = os.listdir(folder_path)
+    file_names = os.listdir(full_path )
     image_names = [name for name in file_names]
     # sort the image names
     sorted_names = sorted(image_names)
     # load the images and store them in a dictionary
     images = {}
     for name in sorted_names:
-        path = os.path.join(folder_path, name)
+        path = os.path.join(full_path , name)
         image = cv2.imread(path)
         images[name] = image
     return images
 
+absolute_path = os.path.dirname(__file__)
+relative_path = "src/lib"
+full_path = os.path.join(absolute_path, relative_path)
